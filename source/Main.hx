978efdfdbfb45bf5bbb14d59fc7478e0920f02d0
DotWithATopHat <52358442+KurtisCates@users.noreply.github.com>
2021-05-17 18:39:49 -0700

    Everybodys fav framerate

--- source/Main.hx
+++ source/Main.hx
@@ -2,7 +2,6 @@ package;
 
 import flixel.FlxGame;
 import flixel.FlxState;
-import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
@@ -14,7 +13,8 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
+	
+	var framerate:UInt = #if web 60 #else 144 #end; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
